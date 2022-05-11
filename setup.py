4127909274fbb6ebb984f17216dc64ba44fb67da
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-11 15:45:53 +0900

    Update setup.py to upload this to pypi

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name='pyqt-svg-icon-text-widget',
@@ -7,10 +14,12 @@ setup(
     author_email='yjg30737@gmail.com',
     license='MIT',
     packages=find_packages(),
-    description='PyQt widget consists of svg icon label and text label',
+    description='PyQt widget consists of svg icon label and text label side by side',
     url='https://github.com/yjg30737/pyqt-svg-icon-text-widget.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     install_requires=[
         'PyQt5>=5.8',
-        'pyqt-svg-label @ git+https://git@github.com/yjg30737/pyqt-svg-label.git@main'
+        'pyqt-svg-label>=0.0.1'
     ]
 )
